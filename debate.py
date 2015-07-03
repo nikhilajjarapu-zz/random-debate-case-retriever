5f5bf55e9b3e3d819e498c09c96841076f1676f9
Nikhil <nikhil@Nikhils-MBP.lan>
2015-07-03 13:59:17 -0700

    Inital commit

---
+++ debate.py
@@ -0,0 +1,38 @@
+import urllib.request
+import random
+import re
+from bs4 import BeautifulSoup
+import os
+os.system('clear')
+cdhtml = urllib.request.urlopen('http://circuitdebater.wikispaces.com/').read()
+soup1 = BeautifulSoup(cdhtml,"html.parser")
+orgpagelinks = soup1.find_all("a", {"class": "wiki_link"})	
+while True:
+	try:
+		randsoup = BeautifulSoup(urllib.request.urlopen(random.choice(orgpagelinks)['href']).read(),"html.parser")
+	except ValueError:
+		try:
+			randsoup = BeautifulSoup(urllib.request.urlopen("http://circuitdebater.wikispaces.com" + 		random.choice(orgpagelinks)['href']).read(),"html.parser")
+		except:
+			continue
+	break
+
+doclinks = randsoup.find_all('a',{"class":"filename"})
+while not doclinks:
+	try:
+		randsoup = BeautifulSoup(urllib.request.urlopen(random.choice(orgpagelinks)['href']).read(),"html.parser")
+
+	except:
+		randsoup = BeautifulSoup(urllib.request.urlopen("http://circuitdebater.wikispaces.com" + random.choice(orgpagelinks)['href']).read(),"html.parser")
+	doclinks = randsoup.find_all('a',{"class":"filename"})
+
+flag = False
+while flag == False:
+	file = random.choice(doclinks)
+	if '/file/view' in file['href']:
+		debatecase = urllib.request.urlopen('http://circuitdebater.wikispaces.com' + file['href'])
+		localfile = open('debatecase.docx','wb')
+		localfile.write(debatecase.read())
+		localfile.close()
+		os.system('open debatecase.docx')
+		flag = True
