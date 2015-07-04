15f7fded191a306067b8d3dfe40bd6f15d6faf1b
Nikhil Ajjarapu <nikhil.ajjarapu@gmail.com>
2015-07-04 11:24:50 -0700

    Added new functionality
    Now the program will also record how fast you spread the case. It will store the times in a text file locally called "debatetimes.txt" and if you happen to download a file you've downloaded before, it will measure whether you did better or worse and record the best times only.

--- debate.py
+++ debate.py
@@ -4,6 +4,8 @@ import re
 from bs4 import BeautifulSoup
 import os
 import sys
+import time
+import string
 
 if sys.version_info[:3] < (3,2,0):
     print('Requires Python >= 3.2.0')
@@ -43,3 +45,26 @@ while flag == False:
 		localfile.close()
 		os.system('open ' + '\'' + file['title'] + '\'')
 		flag = True
+
+print("Press enter when you start spreading. Press enter again once you are done. ")
+input()
+curr = time.time()
+input()
+end = time.time()
+deb8time = end-curr
+flagChanged = False
+with open("debatetimes.txt","a+") as file2:
+	for line in file2:
+		if file['title'] + "," in line:
+			oldarr = line.split(',',2) 
+			print("Your old time for " + orldarr[0] + "was " + oldarr[1])
+			if oldarr[1] > deb8time:
+				print("You beat your record! Good job.")
+				line = ','.join([file['title'],deb8time])
+			else:
+				print("You were faster before. Keep improving!")
+			flagChanged = True
+	if flagChanged == False:
+		print("You haven't spread this case before.")
+		newtime = input("What time did you get? MM:SS ")
+		file2.write(','.join([file['title'],newtime]))
